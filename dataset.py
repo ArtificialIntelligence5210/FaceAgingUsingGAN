53c613faecbbfb2864303fec1f81f0413a6d9db7
ArtificialIntelligence5210 <104875877+ArtificialIntelligence5210@users.noreply.github.com>
2022-05-03 21:45:19 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -3,7 +3,7 @@ import os
 from PIL import Image
 from torch.utils.data import Dataset
 
-
+#We have defined class with argument "Dataset"
 class ImagetoImageDataset(Dataset):
     def __init__(self, domainA_dir, domainB_dir, transforms=None):
         self.imagesA = [os.path.join(domainA_dir, x) for x in os.listdir(domainA_dir) if
@@ -12,10 +12,10 @@ class ImagetoImageDataset(Dataset):
                         x.endswith('.png') or x.endswith('jpg')]
 
         self.transforms = transforms
-
+      # defined method __len__
     def __len__(self):
         return min(len(self.imagesA), len(self.imagesB))
-
+    # Using magic method
     def __getitem__(self, idx):
         imageA = Image.open(self.imagesA[idx])
         imageB = Image.open(self.imagesB[idx])
